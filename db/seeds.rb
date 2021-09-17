8143d422416f1293cbefa7e83c11633c74db2715
Angel <angeljcampo@gmail.com>
2021-09-17 15:04:21 -0300

    adding more seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,7 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-20.times do |i|
+30.times do |i|
   Category.create(
     name: "Category #{i}",
     private: [true,false].sample)
